66aa3edaf4a1d3e67922e98eb6484a772634815a
hannahgurlah <hannahpanares@gmail.com>
2013-04-27 19:42:49 +0800

    rename cdojobs to landingpage and remove asset folder

---
+++ landing-page/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+	<meta charset="utf-8">
+	<title>Cagayan de Oro Jobs</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+	<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
+	<link type="text/css" rel="stylesheet" href="assets/css/visual.css" media="screen" />
+	<link type="text/css" rel="stylesheet" href="assets/css/media-queries.css" media="screen" />
+	<!--[if lt IE 9]>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+		<script src="assets/js/selectivizr-min.js"></script>
+	<![endif]-->
+</head>
+<body>
+
+<!-- Progress bar -->
+<div id="progressbar" data-perc="75">
+	<div class="bar"></div>
+	<div class="label"><span></span></div>
+</div>
+
+<!-- Wrap this content into a container that sets a default width. Progress is outside of this because it's width is 100%. -->
+<div id="container">
+
+
+	<!-- Description text -->
+	<section id="tagline">
+		<h2><b>JOBS</b> Available soon.</h2>
+	</section>
+
+	<section id="logo">
+		<img src="assets/img/cagayandeorojobs-logo.png" alt="cagayan de oro jobs"/>
+		<h1>cagayandeorojobs.com</h1>
+	</section>
+	<div style="clear:both">&nbsp;</div>
+
+	<!-- Subscription form -->
+	<form id="subscribe">
+		<input type="text" id="email" name="email" placeholder="Enter your email" />		
+		 <button>Notify me</button>
+	</form>
+	
+	<!-- Countdown -->	  
+	<section id="countdown"></section>
+
+
+	<!-- Social media icons -->
+	<section id="social">
+		<ul>
+			<li class="facebook"><a href="#" title="Be a facebook fan">Fan</a></li>
+			<li class="twitter"><a href="#" title="Follow us on twitter">Follow</a></li>
+			<li class="google"><a href="#" title="Befriend us on Google">Circle</a></li>
+		</ul>
+	</section>
+	
+	<!-- Copyright message -->
+
+
+</div>
+
+<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'></script>
+<script src="assets/js/core.js"></script>
+<script src="assets/js/tweet.js"></script>
+<script src="assets/js/countdown.min.js"></script>
+
+</body>
+</html>
